65607b3556005767656884faa33db7cb102298cc
Carlos Perea <carlos.perea.figuera.al@iespoligonosur.org>
2025-01-23 11:20:50 +0100

    terminar

--- proyectoModeloClienteAPI/clienteApi/views.py
+++ proyectoModeloClienteAPI/clienteApi/views.py
@@ -10,8 +10,10 @@ def pagina_de_enlaces(request):
     return render(request, 'enlaces.html')  
 
 def tutorial_lista_api (request):
+    #Le damos permiso de autorizacion
+    headers = {'Authorization': 'Bearer G8S54YVURkTo8oS9fp7VF9fVnYQpnU'}
     #Obtenemos todos los tutoriales de la api primero
-    response = requests.get('http://127.0.0.1:8000/api/v1/tutorial')
+    response = requests.get('http://127.0.0.1:8000/api/v1/tutorial', headers= headers)
     #Transformamos la repsuesta en JSON
     tutoriales = response.json()
     return render(request, 'Tutorial/lista_tutorial_api.html', {
